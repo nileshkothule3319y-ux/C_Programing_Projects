93d649c050a5ca957492674a6dcc55c38bbf8b1b
nileshkothule3319y-ux <nileshkothule3319y@gmail.com>
2026-01-19 20:34:53 +0530

    progress_bar.c

--- progress_bar.c
+++ progress_bar.c
@@ -1,78 +1,66 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<stdlib.h>
-#include<time.h>
- const int BAR_len =50;
- const int Max_task=5;
- 
- typedef struct 
- {
-    int id;
-    int progress;
-    int step;
-    
- }Task;
- void BAR_function(Task task);
- void screen_clear();
+#include <stdio.h>   // For printf()
+#include <unistd.h>  // For sleep()
+#include <stdlib.h>  // For rand(), srand()
+#include <time.h>    // For time()
+
+/* Length of the progress bar */
+const int BAR_len = 50;
+
+/* Total number of tasks */
+const int Max_task = 5;
+
+/* Structure to store task information */
+typedef struct
+{
+    int id;        // Task ID number
+    int progress;  // Current progress (0 to 100)
+    int step;      // Speed of progress increment
+} Task;
+
+/* Function declarations */
+void BAR_function(Task task);
+void screen_clear(void);
+
 int main()
 {
-   Task  task[Max_task];
-   srand(time(NULL));
-for(int i=0;i<Max_task;i++ )
-{
-task[i].id=i+1;
-task[i].progress=0;
-task[i].step=rand()%5+1;
-}
-int Task_incomplet =1;
-while(Task_incomplet)
-{Task_incomplet=0;
-screen_clear();
-for(int i=0;i<Max_task;i++)
-{
-    task[i].progress += task[i].step;
-if (task[i].progress>100)
-{
-    task[i].progress=100;
-}
-else if(task[i].progress<100)
+    /* Array of tasks */
+    Task task[Max_task];
+
+    /* Seed random number generator */
+    srand(time(NULL));
+
+    /* Initialize all tasks */
+    for (int i = 0; i < Max_task; i++)
     {
-        Task_incomplet=1;
-    }  
-BAR_function(task[i]);
-}
-sleep(1);  //sleep for 1 sec
-}
-printf("All tasks completed");
-return 0;
-}
-void BAR_function(Task task)
-{
-int bar_progrss = (BAR_len*task.progress)/100;
-printf("Task %d: [",task.id);
-for(int i=0; i<BAR_len;i++)
-{
-if(i<bar_progrss)
-{
-    printf("=");
-}
-else
-{
-    printf(" ");
-}
-}
-printf("] %d%%\n",task.progress);
-}
-void screen_clear()
-{
+        task[i].id = i + 1;            // Assign task ID
+        task[i].progress = 0;          // Start progress at 0%
+        task[i].step = rand() % 5 + 1; // Random speed (1 to 5)
+    }
 
+    int Task_incomplet = 1;  // Flag to check if any task is incomplete
 
-    printf("\033[2J\033[H");
-    fflush(stdout);
-}
+    /* Loop until all tasks reach 100% */
+    while (Task_incomplet)
+    {
+        Task_incomplet = 0;   // Assume all tasks are complete
+        screen_clear();       // Clear terminal screen
+
+        /* Update and display each task */
+        for (int i = 0; i < Max_task; i++)
+        {
+            task[i].progress += task[i].step; // Increase progress
+
+            /* Limit progress to maximum 100% */
+            if (task[i].progress > 100)
+            {
+                task[i].progress = 100;
+            }
+            /* If any task is still incomplete, continue loop */
+            else if (task[i].progress < 100)
 
 
 
     
+
 
 
